08b434144227bdd0c2b3ce577f6364e3bff784b0
buthed <temaTihonov@yandex.ru>
2021-03-28 23:26:10 +0300

    Initial commit

---
+++ src/com/tematihonov/Main.java
@@ -0,0 +1,14 @@
+package com.tematihonov;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Дз №5. Рекрсия'");
+        System.out.println("Task 5.1");
+        System.out.println("Пример использвания рекурсии: ");
+
+        System.out.println("Task 5.2");
+        System.out.println("Реализация бесконечной и обычной рекурсии с условием для входа: ");
+        System.out.println("Обычная рекурсия");
+    }
+}
